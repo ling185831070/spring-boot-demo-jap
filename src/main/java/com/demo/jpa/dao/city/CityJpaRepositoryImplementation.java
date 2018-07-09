eb147badad62d7471b876ee067c2d970e8ec857e
zhanghaifeng <zhanghaifeng@51dxwl.com>
2018-07-09 11:23:47 +0800

    domain

--- src/main/java/com/demo/jpa/dao/city/CityJpaRepositoryImplementation.java
+++ src/main/java/com/demo/jpa/dao/city/CityJpaRepositoryImplementation.java
@@ -3,5 +3,10 @@ package com.demo.jpa.dao.city;
 import com.demo.jpa.domian.City;
 import org.springframework.data.jpa.repository.support.JpaRepositoryImplementation;
 
+/**
+ * 继承此类可以得到默认的操作方法
+ */
+
+
 public interface CityJpaRepositoryImplementation extends JpaRepositoryImplementation<City,Long> {
 }
--- src/main/java/com/demo/jpa/dao/city/CityRepository.java
+++ src/main/java/com/demo/jpa/dao/city/CityRepository.java
@@ -18,9 +18,14 @@ package com.demo.jpa.dao.city;
 
 
 import com.demo.jpa.domian.City;
+import com.demo.jpa.domian.HotelSummary;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
@@ -30,10 +35,48 @@ import java.util.List;
  * 后面跟属性名称，关键字之类的
  */
 interface CityRepository extends Repository<City, Long>{
-
+	//==============》分页查询
 	Page<City> findAll(Pageable pageable);
+
 	Page<City> findByNameContainingAndCountryContainingAllIgnoringCase(String name, String country, Pageable pageable);
+	//《==============分页查询
+
+
+	//==============》限制查询
 	City findByNameAndCountryAllIgnoringCase(String name, String country);
 
+	City findFirstByOrderByLastNameAsc();
+
+	City findTopByOrderByAgeDesc();
+
+	Page<City> queryFirst10ByLastname(String lastname,Pageable pageable);
+
+	List<City> findFirst10ByLastname(String lastname,Sort sort);
+
+	List<City> findTop10ByLastname(String lastname,Pageable pageable);
+	//《==============限制查询
+
+	//==============》自定义SQL查询
+	@Modifying
+	@Query("update City c set c.name=?1 where c.id= ?2")
+	int modifyByIdAndUserId(String name,Long id);
+
+	@Transactional
+	@Modifying
+	@Query("delete  from City where id= ?1")
+	void deleteByCityId(Long id);
+
+	@Transactional(timeout = 10)
+	@Query("select u from City c where c.country= ?1")
+	City findByCountry(String country);
+	//《==============自定义SQL查询
+
+	//==============》多表查询
+	@Query("select h.city as city, h.name as name, avg(r.rating) as averageRating from Hotel h left outer join h.reviews r where h.city= ?1 group by h")
+	Page<HotelSummary> findByCity(City city,Pageable pageable);
+
+	@Query("select h.name as name,avg(r.rating) as averageRating from Hotel h left outer join h.reviews r group by h")
+	Page<HotelSummary> findByCity(Pageable pageable);
+	//《==============多表查询
 
 }
